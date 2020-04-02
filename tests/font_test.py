df6b3bf32de594ee8c41f2a1b4e1c613c40a4be9
pagesofpages <gmenchen@gmail.com>
2020-04-02 17:03:00 -0400

    First pass at migrating to python3

--- tests/font_test.py
+++ tests/font_test.py
@@ -13,59 +13,12 @@ doc.beginDoc('font_test.pdf')
 
 doc.newPage()
 doc.setTextState()
-doc.setFont('F3',8)
 
-doc.write(10,10,'This is an 8 point font')
-doc.setDefaultUnit('r')
-doc.write(0,0,'This is 0,0 with an 8 point font','l','r')
-br = doc.getBottomRow('r')
-rc = doc.getRightColumn('c')
-doc.write(rc,br,'This is '+str(rc)+','+str(br)+' with an 8 point font','r','r')
-br = doc.getBottomRow('p') - 10
-rc = doc.pageWidth - doc.leftMargin 
-doc.write( rc, br,'One line up, the actual raw right margin', 'r','p')
-doc.write(doc.getBottomRow() - 3, doc.getRightColumn(),'X','l','c')
-ruler = ''
+for fontsize in range(8,14):
+	doc.setFont('F3',fontsize)
+	doc.write(10,fontsize,"this is a "+str(fontsize)+" point font at 10,"+str(fontsize),'l','r')
+doc.endPage()	
 
-for i in range(0,20):
-	ruler = ruler + '1234567890'
-
-	
-doc.write(0,20,ruler,'l','r')
-doc.write(117,21,'X','l','r')	
-
-doc.write(0,30,'Bottom Row '+str(doc.getBottomRow()),'l')
-doc.write(0,31,'Right Column '+str(doc.getRightColumn()),'l')
-doc.endPage()
-
-
-doc.newPage()
-doc.setPageSize(8.5, 11.0, 'i')
-doc.setTextState()
-doc.setFont('F3',8)
-
-doc.write(10,10,'This is an 8 point font')
-doc.setDefaultUnit('r')
-doc.write(0,0,'This is 0,0 with an 8 point font','l','r')
-br = doc.getBottomRow('r')
-rc = doc.getRightColumn('c')
-doc.write(rc,br,'This is '+str(rc)+','+str(br)+' with an 8 point font','r','r')
-br = doc.getBottomRow('p') - 10
-rc = doc.pageWidth - doc.leftMargin 
-doc.write( rc, br,'One line up, the actual raw right margin', 'r','p')
-doc.write(doc.getBottomRow() - 3, doc.getRightColumn(),'X','l','c')
-ruler = ''
-
-for i in range(0,20):
-	ruler = ruler + '1234567890'
-
-	
-doc.write(0,20,ruler,'l','r')
-doc.write(doc.getRightColumn(),21,'X','l','r')	
-
-doc.write(0,30,'Bottom Row '+str(doc.getBottomRow()),'l')
-doc.write(0,31,'Right Column '+str(doc.getRightColumn()),'l')
-doc.endPage()
 
 
 
@@ -73,4 +26,4 @@ doc.endPage()
 doc.endDoc()
 
 
-print "See font_test.pdf"
+print( "See font_test.pdf" )
